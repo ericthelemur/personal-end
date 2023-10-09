ab0a114dd90024631c9d73820010a69aacf15fbd
ericthelemur <owenconnors1@gmail.com>
2023-10-10 00:30:39 +0100

    Portal tailgating

--- personalend/src/main/java/ericthelemur/personalend/mixin/EndPortalBlockMixin.java
+++ personalend/src/main/java/ericthelemur/personalend/mixin/EndPortalBlockMixin.java
@@ -1,5 +1,6 @@
 package ericthelemur.personalend.mixin;
 
+import ericthelemur.personalend.DragonPersistentState;
 import ericthelemur.personalend.PersonalEnd;
 import net.fabricmc.fabric.api.dimension.v1.FabricDimensions;
 import net.minecraft.block.BlockState;
@@ -19,20 +20,39 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import xyz.nucleoid.fantasy.Fantasy;
-import xyz.nucleoid.fantasy.RuntimeWorldConfig;
-import xyz.nucleoid.fantasy.RuntimeWorldHandle;
+
+import java.util.UUID;
 
 @Mixin(EndPortalBlock.class)
 public class EndPortalBlockMixin {
+	private static long lastEntryTime;
+	private static UUID lastPlayer;
+	private static final long trailTime = 15 * 1000;
+
 	@Inject(at = @At("HEAD"), method = "onEntityCollision", cancellable = true)
 	private void disableEndPortal(BlockState state, World world, BlockPos pos, Entity entity, CallbackInfo ci) {
 		MinecraftServer server = entity.getServer();
 		if (entity.isPlayer() && world.getRegistryKey() == World.OVERWORLD) {
-			PersonalEnd.genAndGoToEnd((PlayerEntity) entity, entity.getUuid(), null);
+			PersonalEnd.LOGGER.info("Joining world as {} {}", entity.getName().getString(), server.getTickTime());
+			var owner = getDimOwner(entity);
+			var dstate = DragonPersistentState.getServerState(server);
+			PersonalEnd.genAndGoToEnd((PlayerEntity) entity, owner, dstate.getUsername(owner));
+			ci.cancel();
 		} else if (world.getDimensionKey().getValue() == DimensionTypes.THE_END.getValue()) {
 			PersonalEnd.tpToOverworld(entity, server);
+			ci.cancel();
 		}
-		ci.cancel();
+	}
+
+	private UUID getDimOwner(Entity entity) {
+		long t = System.currentTimeMillis();
+		PersonalEnd.LOGGER.info("Times base {} limit {} current {} last {}", lastEntryTime, lastEntryTime + trailTime, t, lastPlayer);
+		UUID owner = entity.getUuid();
+		if (lastPlayer != null && t < lastEntryTime + trailTime) {
+			owner = lastPlayer;
+		}
+		lastEntryTime = t;
+		lastPlayer = owner;
+		return owner;
 	}
 }
